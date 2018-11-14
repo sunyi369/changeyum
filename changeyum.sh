c9e6a5ce234095eeb746fc91e277b111c900acdb
zhaorui <25546385+zhaorui963@users.noreply.github.com>
2018-11-14 22:58:14 +0800

    Update changeyum.sh

--- changeyum.sh
+++ changeyum.sh
@@ -4,7 +4,7 @@ rm -rf /etc/yum.repos.d/* &&
 cd /root/changeyum/rpm-gpg &&
 cp -f * /etc/pki/rpm-gpg/ &&
 cd /root/changeyum/yum.repos.d &&
-cp -f * /etc/yum.repos.d/* &&
+cp -f * /etc/yum.repos.d/ &&
 yum clean all &&
 yum makecache &&
 echo '全部完成！'
